0520615a0537d7cd616861b6b94d7396e1e887b9
jinnycho <jinnycho.eng@gmail.com>
2019-07-23 00:11:50 -0400

    Start src

---
+++ book_recommendation_2.0/src/main/scala/BookRec.scala
@@ -0,0 +1,16 @@
+package com.jinnycho503.spark
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+import scala.io.Source
+import java.nio.charset.CodingErrorAction
+import scala.io.Codec
+import scala.math.sqrt
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+object BookRec {
+
+}
