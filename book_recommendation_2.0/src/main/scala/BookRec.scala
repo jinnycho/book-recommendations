6726ef55b22a236b394d162e9a06bdde9465cb76
jinnycho <jinnycho.eng@gmail.com>
2019-07-27 03:08:48 -0400

    Updated scala that returns concatenated ratings & ISBN

--- book_recommendation_2.0/src/main/scala/BookRec.scala
+++ book_recommendation_2.0/src/main/scala/BookRec.scala
@@ -13,21 +13,59 @@ import org.apache.spark.sql.functions._
 
 object BookRec {
 
+  type BookRating = (Int, Double)
+  type UserRatingPair = (String, (BookRating, BookRating))
+  def filterDuplicates(userRatings:UserRatingPair):Boolean = {
+    val bookRating1 = userRatings._2._1
+    val bookRating2 = userRatings._2._2
+    val bookISBN1 = bookRating1._1
+    val bookISBN2 = bookRating2._1
+    return (bookISBN1 == bookISBN2) && (bookRating1 == bookRating2)
+  }
+
+  def makePairs(userRatings:UserRatingPair) = {
+    val bookRating1 = userRatings._2._1
+    val bookRating2 = userRatings._2._2
+    val book1 = bookRating1._1
+    val rating1 = bookRating1._2
+    val book2 = bookRating2._1
+    val rating2 = bookRating2._2
+
+    ((book1, book2), (rating1, rating2))
+  }
+
   /*
    * @param {list} args - the list of ISBNs you read
    */
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val sc = new SparkContext("local[*]", "MovieSimilarities")
+    val spark = SparkSession
+      .builder
+      .appName("Book Recommendations")
+      .master("local[*]")
+      .getOrCreate()
 
-    val data = sc.textFile("../data/sample.csv")
+    val customSchema = StructType(Array(
+      StructField("index", IntegerType, true),
+      StructField("userID", StringType, true),
+      StructField("ISBN", StringType, true),
+      StructField("rating", DoubleType, true)))
 
-    // Map ratings to userID => ISBN, rating
-    val ratings = data.map(l => l.split(",")).map(l => (l(0), (l(2).toInt, l(3).toDouble)))
+    val ratingsDF = spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("charset", "UTF8")
+      .schema(customSchema)
+      .load("./data/sample.csv")
 
-    // join by the same user
-    // will create RDD of userID => ((ISBN, rating), (ISBN, rating))
-    val joinedRatings = ratings.join(ratings)
+    // Map ratings to userID => (movie1, movie2)
+    // userID => (rating1, rating2)
+    val joinedISBNDF = ratingsDF.groupBy("userID").agg(collect_list("ISBN").as("ISBN"))
+    val joinedRatingsDF = ratingsDF.groupBy("userID").agg(collect_list("rating").as("rating"))
+    joinedISBNDF.show()
+    joinedRatingsDF.show()
+
+    spark.stop()
   }
 }
