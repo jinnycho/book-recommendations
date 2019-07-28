df8d23ad79beead81b4eda6e6a37765a9ad2a64c
jinnycho <jinnycho.eng@gmail.com>
2019-07-28 15:47:31 -0400

    filtering is somehow working

--- book_recommendation_2.0/src/main/scala/BookRec.scala
+++ book_recommendation_2.0/src/main/scala/BookRec.scala
@@ -13,6 +13,17 @@ import org.apache.spark.sql.functions._
 
 object BookRec {
 
+  type BookRating = (String, Double)
+  type UserRatingPair = (String, (BookRating, BookRating))
+  def filterDuplicates(userRatings:UserRatingPair):Boolean = {
+    val bookRating1 = userRatings._2._1
+    val bookRating2 = userRatings._2._2
+
+    val book1 = bookRating1._1
+    val book2 = bookRating2._1
+    return book1 != book2
+  }
+
   /*
    * @param {list} args - the list of ISBNs you read
    */
@@ -24,6 +35,9 @@ object BookRec {
     val data = sc.textFile("./data/sample.csv")
     // userID => ISBN, rating
     val ratings = data.map(l => l.split(",")).map(l => (l(1), (l(2), l(3).toDouble)))
-    ratings.collect().foreach(println)
+    // Find every pair of books that were read by the same person
+    val joinedRatings = ratings.join(ratings)
+    // Filter out duplicate pairs
+    val uniqueJoinedRatings = joinedRatings.filter(filterDuplicates)
   }
 }
